ed94b6e3031d6ac295d26def15314792c2548292
Asterios <steliospao1@gmail.com>
2021-10-12 04:05:34 +0300

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
+const BodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+/*app.use(BodyParser.urlencoded({
+  extended: true
+}));
+app.use(BodyParser.json());*/
+
 //simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to Asterios application." });
 });
 
+//import routes
+const apiRoutes = require("./routes/api-routes");
+//Use Api routes in the App (api-routes)
+app.use('/user', apiRoutes);
+
 //connect to mongodb
+//process.env.DATABASE
 mongoose.connect(process.env.DATABASE, () =>
     console.log('Connect to DB.')
 );
