3fd18b15bcd0052ad372089f3ab9e313f2d2fb12
Asterios <steliospao1@gmail.com>
2021-10-12 04:43:38 +0300

    get, post, update, delete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,14 +2,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
-//Set default API response
-router.get('/', (req, res) => {
-    res.json({
+//Get all users from database
+router.get('/', async (req, res) => {
+    /*res.json({
         status: 'API Its Working',
         message: 'Welcome to RESTAPI by Asterios!'
-    });
+    });*/
+
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch {
+      res.json({message : "Error"});
+    } 
 });
 
+//Add a user in database
 router.post('/', async (req, res) => {
     console.log(req.body);
     const user = new User({
@@ -22,6 +30,36 @@ router.post('/', async (req, res) => {
     } catch {
       res.json({message : "Error"});
     } 
+});
+
+//Get a specific user from database
+router.get('/:userID', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userID);
+      res.json(user);
+    } catch {
+      res.json({message : "Error"});
+    } 
+});
+
+//Delete a specific user from database
+router.delete('/:userID', async (req, res) => {
+  try {
+    const deleteduser = await User.remove({_id: req.params.userID});
+    res.json(deleteduser);
+  } catch {
+    res.json({message : "Error"});
+  } 
+});
+
+//Update a specific post in database
+router.patch('/:userID', async (req, res) => {
+  try {
+    const updateduser = await User.updateOne({_id: req.params.userID}, {$set: {name: req.body.name}});
+    res.json(updateduser);
+  } catch {
+    res.json({message : "Error"});
+  } 
 });
 
 //Export API routes
